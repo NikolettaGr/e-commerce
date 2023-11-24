130977cd22a6b23cf86f69e5dd624079c4dd682d
NikolettaGr <grishaberN@outlook.com>
2023-11-24 21:23:01 +0100

    Adjust the quantity of the specified product

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 
 def view_cart(request):
@@ -20,3 +20,18 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id, None)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))

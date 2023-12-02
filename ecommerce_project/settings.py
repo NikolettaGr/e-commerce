ad507d82e8195056aad1f3aba45b758397a9e83a
NikolettaGr <grishaberN@outlook.com>
2023-12-02 16:14:12 +0100

    Add wishlist view functionality, url and template

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'checkout',
     'crispy_forms',
     'profiles',
+    'wishlist',
 ]
 
 MIDDLEWARE = [
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,88 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Wishlist
+from products.models import Product
 
-# Create your views here.
+
+@login_required
+def view_wishlist(request):
+    """ A view that renders the cart contents page """
+    user = request.user
+    wishlist, created = Wishlist.objects.get_or_create(user=user)
+    products = Wishlist.objects.filter(user=user)
+    wishlist_data = {
+        'user_id': user.id,
+        'wishlist_id': wishlist.id,
+        'products': products,
+        'created_at': wishlist.created_at,
+    }
+    return render(request, 'wishlist/wishlist.html', wishlist_data)
+
+
+@login_required
+def add_to_wishlist(request, product_id):
+    user = request.user
+    wishlist, created = Wishlist.objects.get_or_create(user=user)
+    product = get_object_or_404(Product, pk=product_id)
+    wishlist.products.add(product) 
+    wishlist.save()  
+    wishlist_data = {
+        'user_id': user.id,
+        'wishlist_id': wishlist.id,
+        'products': wishlist.products.all(),  
+        'created_at': wishlist.created_at,
+    }
+    return render(request, 'wishlist/wishlist.html', {'wishlist_data': wishlist_data})
+
+
+# @login_required
+# @require_POST
+# def add_to_wishlist(request, product_id):
+#     user = request.user
+#     wishlist, created = Wishlist.objects.get_or_create(user=user)
+#     product = get_object_or_404(Product, pk=product_id)
+
+    
+#     wishlist.products.add(product)
+#     wishlist.save()
+#     messages.success(request, 'Item added to your wishlist.')
+
+#     test = wishlist.products.all()
+
+#     context = {
+#       'wishlist': test,
+#     }
+
+#     return render(request, 'wishlist/wishlist.html', context)
+    
+
+# @login_required
+# @require_POST
+# def remove_from_wishlist(request, product_id):
+#     user = request.user
+#     wishlist, created = Wishlist.objects.get_or_create(user=user)
+#     product = get_object_or_404(Product, pk=product_id)
+
+    
+#     wishlist.products.remove(product)
+#     wishlist.save()
+#     messages.success(request, 'Item removed from your wishlist.')
+
+def remove_from_wishlist(request, product_id):
+    user = request.user
+    wishlist, created = Wishlist.objects.get_or_create(user=user)
+    product = get_object_or_404(Product, pk=product_id)
+
+    wishlist.products.remove(product)
+    wishlist.save()
+
+    wishlist_data = {
+        'user_id': user.id,
+        'wishlist_id': wishlist.id,
+        'product': product,
+        'created_at': wishlist.created_at,
+    }
+
+    return render(request, 'wishlist/wishlist.html', {'wishlist_data': wishlist_data})

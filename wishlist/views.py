3fc1cef9f921cc9dd645c00bad5dcf1a692f328e
NikolettaGr <grishaberN@outlook.com>
2023-12-03 16:41:18 +0100

    Fix add to wishlist function and template

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -26,17 +26,22 @@ def add_to_wishlist(request, product_id):
     user = request.user
     wishlist, created = Wishlist.objects.get_or_create(user=user)
     product = get_object_or_404(Product, pk=product_id)
-    wishlist.products.add(product) 
-    wishlist.save()  
+
+    # Check if the product is already in the wishlist
+    if product in wishlist.products.all():
+        messages.warning(request, 'This item is already in your wishlist.')
+    else:
+        wishlist.products.add(product)
+        wishlist.save()
+        messages.success(request, 'Item added to wishlist successfully.')
+
     wishlist_data = {
         'user_id': user.id,
         'wishlist_id': wishlist.id,
-        'products': wishlist.products.all(),  
+        'products': wishlist.products.all(),
         'created_at': wishlist.created_at,
     }
-    return render(
-        request, 'wishlist/wishlist.html', {'wishlist_data': wishlist_data})
-
+    return redirect('product_detail', product_id=product.id)
 
 def remove_from_wishlist(request, product_id):
     user = request.user

cdf5a64c6c397ccec6510cbd95f2d3bd5cb51013
NikolettaGr <grishaberN@outlook.com>
2023-12-05 10:56:40 +0100

    Fix settings for static files

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -201,9 +201,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Stripe 
 
